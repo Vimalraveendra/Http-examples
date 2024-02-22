d536f19be6c541435424bc185cdece7131a8131e
Vimal Raveendran <vimalraveendra@gmail.com>
2024-02-22 13:48:17 +0100

    added interceptors,auth-interceptor & logging-interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from "@angular/common/http"
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http"
+import { AuthInterceptorService } from './auth-interceptor.service';
+import { LoggingInterceptorService } from './logging-interceptor.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,18 @@ import {HttpClientModule} from "@angular/common/http"
     FormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+    provide:HTTP_INTERCEPTORS,
+    useClass:AuthInterceptorService,
+    multi:true
+  },
+  {
+    provide:HTTP_INTERCEPTORS,
+    useClass:LoggingInterceptorService,
+    multi:true
+  }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
