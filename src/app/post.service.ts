6385f8e412f0885ed59aec50c52d3c3377a09246
Vimal Raveendran <vimalraveendra@gmail.com>
2024-02-22 13:05:27 +0100

    added setting headers, adding query parameters,observing different types of response & changing the response body type of http modules

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
-import { map } from "rxjs";
+import { map, tap } from "rxjs";
 
 @Injectable({
     providedIn:'root'
@@ -11,11 +11,24 @@ export class PostService {
    createAndStorePost(title:string,content:string){
     const postData:Post={title:title,content:content}
     return this.http.post<{name:string}>('https://angular-recipe-c13c6-default-rtdb.firebaseio.com/posts.json',
-    postData)
+    postData,
+    {
+        observe:'response'
+    }
+    )
 }
 
    fetchPosts(){
-    return this.http.get('https://angular-recipe-c13c6-default-rtdb.firebaseio.com/posts.json')
+    let searchParams = new HttpParams()
+    searchParams= searchParams.append('print','pretty');
+    searchParams=searchParams.append('custom','key');
+    return this.http.get('https://angular-recipe-c13c6-default-rtdb.firebaseio.com/posts.json',
+    {
+        headers:new HttpHeaders({'Custom-Header':'Hello'}),
+        // params:new HttpParams().set('print','pretty')
+        params:searchParams
+    }
+    )
    .pipe(map((responseData:{[key:string]:Post})=>{
     const postArray:Post[]=[];
     for(const  key in responseData){
@@ -26,6 +39,19 @@ export class PostService {
    }
 
    deletePosts(){
-    return this.http.delete ('https://angular-recipe-c13c6-default-rtdb.firebaseio.com/posts.json')
+    return this.http.delete ('https://angular-recipe-c13c6-default-rtdb.firebaseio.com/posts.json',
+    {
+        observe:'events',
+        responseType:'json'
+    }
+    ).pipe(tap(event=>{
+        console.log("event",event)
+        if(event.type===HttpEventType.Sent){
+
+        }
+        if(event.type===HttpEventType.Response){
+            console.log('eventBody',event.body)
+        }
+    }))
    }
 }
